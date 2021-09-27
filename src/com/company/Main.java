fbe7d3e74dfb8ddc68d8d86074ed77b33fc35e47
Powel Jimenez Fernando <mon_mail>
2021-09-27 15:04:53 +0200

    salut

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String Scanner = "";
+        System.out.println("Nom et prénom");
+        java.util.Scanner sc;
+        sc = new Scanner(System.in);
+        String i = sc.nextLine();
+
+
+        System.out.println("Hello " + i);
+    }
+}
